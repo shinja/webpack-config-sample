30e770a21a9fb3688956a0dc087bc85f2ad4bb11
shinja <shinja.dev@gmail.com>
2017-05-28 11:04:03 +0800

    Apply webpack UglifyJsPlugin. Apply html-webpack-plugin for webpack inject bundle.js/vendor.bundle.js in index.html.

--- webpack.vendor.js
+++ webpack.vendor.js
@@ -1,16 +1,24 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
 var webpack = require("webpack");
 
 module.exports = {
   entry: {
     app: "./app.js",
-    vendor: ["jquery", "underscore"], 
+    vendor: ["jquery", "underscore"],
   },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' })
+    new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html')
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.bundle.js'
+    })
   ]
 };
